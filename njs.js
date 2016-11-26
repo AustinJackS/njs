c4cc97f9d0fafd2f6911e861b002eb082b015b62
AustinJackS <austinjack404@gmail.com>
2016-11-26 13:06:16 -0500

    Update njs.js

--- njs.js
+++ njs.js
@@ -3,5 +3,6 @@ function njinit() {
   nscode = nscode.replace('ajax','function loadDoc() {var xhttp = new XMLHttpRequest();xhttp.onreadystatechange = function() {if (this.readyState == 4 && this.status == 200) {window.ajaxresp = this.responseText;}};xhttp.open');
   nscode = nscode.replace('$ajax',';xhttp.send();}');
   document.getElementsByClassName('nscript')[0].innerHTML = nscode;
-  eval(nscode);
+  nsinit = new Function(nscode);
+  nsinit();
 }
