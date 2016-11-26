67a8407d700ecda3970f0a9ebc638dbd834f7208
AustinJackS <austinjack404@gmail.com>
2016-11-26 15:48:23 -0500

    Update xscript.js

--- xscript.js
+++ xscript.js
@@ -4,8 +4,8 @@ for (i = 0; i < scs.length; i++) {
         cde = scs[i].innerText;
     	cde = cde.replace(/display/g,'alert');
         cde = cde.replace(/alrt/g,'alert');
-        cde = cde.replace(/ajax/g,'var xhttp = new XMLHttpRequest(); xhttp.onreadystatechange = function() { if (this.readyState == 4 && this.status == 200) { window.ajaxresp = this.responseText; } }; xhttp.open(');
-        cde = cde.replace(/-ajax/g,'); xhttp.send();');
+        cde = cde.replace(/ajax/g,'var xhttp = new XMLHttpRequest(); xhttp.onreadystatechange = function() { if (this.readyState == 4 && this.status == 200) { window.resp = this.responseText; } }; xhttp.open');
+        cde = cde.replace(/-ajax/g,'; xhttp.send();');
         fn = new Function(cde);
         fn();
     }
