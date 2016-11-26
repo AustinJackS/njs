56de91cbab994a26b42121f42e08d4d9eb4c5f87
AustinJackS <austinjack404@gmail.com>
2016-11-26 15:18:29 -0500

    Update xscript.js

--- xscript.js
+++ xscript.js
@@ -1,8 +1,8 @@
 var scs = document.getElementsByTagName('script');
 for (i = 0; i < scs.length; i++) {
     if (scs[i].type=="text/xscript") {
-    	cde = scs[i].innerText.replace(/display/g,'alert')
+    	cde = scs[i].innerText.replace(/display/g,'alert');
+        fn = new Function(cde);
+        fn();
     }
 }
-fn = new Function(cde)
-fn();
