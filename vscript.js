dd91b9db1cbe27f339cf9bf4ff83fe42c753a543
AustinJackS <austinjack404@gmail.com>
2016-11-26 15:11:46 -0500

    Update vscript.js

--- vscript.js
+++ vscript.js
@@ -1,7 +1,7 @@
 var scs = document.getElementsByTagName('script');
 for (i = 0; i < scs.length; i++) {
     if (scs[i].type=="text/vscript") {
-    	cde = scs[i].innerText.replace('display','alert')
+    	cde = scs[i].innerText.replace(/display/g,'alert')
     }
 }
 fn = new Function(cde)
