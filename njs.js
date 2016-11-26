e543beab6d35ba99007c8ee427f816b5e8ab1475
AustinJackS <austinjack404@gmail.com>
2016-11-26 12:24:36 -0500

    Update njs.js

--- njs.js
+++ njs.js
@@ -1,1 +1,6 @@
-
+function njinit(cnum) {
+  var njsc = document.getElementsByClassName('njs')[cnum].innerText;
+  njsc = njsc.replace('ajax','function loadDoc() {var xhttp = new XMLHttpRequest();xhttp.onreadystatechange = function() {if (this.readyState == 4 && this.status == 200) {window.ajaxresp = this.responseText;}};xhttp.open');
+  njsc = njsc.replace('$ajax',';xhttp.send();}');
+  eval(njsc);
+}
