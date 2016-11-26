608fffe962456f7280a83cfde6144ffc2d3c21f8
AustinJackS <austinjack404@gmail.com>
2016-11-26 16:06:03 -0500

    Delete njs.js

--- njs.js
+++
@@ -1,8 +0,0 @@
-function njinit() {
-  var nscode = document.getElementsByClassName('nscript')[0].innerHTML
-  nscode = nscode.replace('ajax','function loadDoc() {var xhttp = new XMLHttpRequest();xhttp.onreadystatechange = function() {if (this.readyState == 4 && this.status == 200) {window.ajaxresp = this.responseText;}};xhttp.open');
-  nscode = nscode.replace('$ajax',';xhttp.send();}');
-  document.getElementsByClassName('nscript')[0].innerHTML = nscode;
-  nsinit = new Function(nscode);
-  nsinit();
-}
